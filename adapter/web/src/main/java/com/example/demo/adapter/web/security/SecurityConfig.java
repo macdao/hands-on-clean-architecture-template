e16a06ad65d61e86903533821f862a3d4d9298c6
Xi Qi <xqi@thoughtworks.com>
2024-09-29 11:58:39 +0800

    feat: make web processing stateless

--- adapter/web/src/main/java/com/example/demo/adapter/web/security/SecurityConfig.java
+++ adapter/web/src/main/java/com/example/demo/adapter/web/security/SecurityConfig.java
@@ -6,10 +6,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
+import org.springframework.security.web.context.NullSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
 
 @Configuration
 public class SecurityConfig {
@@ -18,6 +21,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .securityContext(securityContext ->
+                        securityContext.securityContextRepository(new RequestAttributeSecurityContextRepository()))
                 .authenticationProvider(preAuthenticatedAuthenticationProvider())
                 .with(new RequestHeaderAuthenticationFilterConfigurer(), Customizer.withDefaults())
                 .build();
@@ -39,6 +45,7 @@ public class SecurityConfig {
             filter.setAuthenticationManager(authenticationManager);
             filter.setPrincipalRequestHeader("Authorization");
             filter.setExceptionIfHeaderMissing(false);
+            filter.setSecurityContextRepository(new NullSecurityContextRepository());
             http.addFilter(filter);
         }
     }
