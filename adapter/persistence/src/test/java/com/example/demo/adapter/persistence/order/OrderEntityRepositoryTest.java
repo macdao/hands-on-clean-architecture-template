08c1fdc87888c23fc8d1129905bd59a558655bea
Xi Qi <xqi@thoughtworks.com>
2025-06-21 23:24:47 +0800

    test: use DataJpaTest instead of SpringBootTest

--- adapter/persistence/src/test/java/com/example/demo/adapter/persistence/order/OrderEntityRepositoryTest.java
+++ adapter/persistence/src/test/java/com/example/demo/adapter/persistence/order/OrderEntityRepositoryTest.java
@@ -8,12 +8,10 @@ import java.math.BigDecimal;
 import java.time.Instant;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-@Transactional
+@DataJpaTest
 class OrderEntityRepositoryTest {
 
     @Autowired
